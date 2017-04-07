aebcc884138bd7dd5b8b88167388a32b9e941a08
howardliu-cn <liuxinghao1988@gmail.com>
2017-04-07 15:56:21 +0800

    netty nio demo: fixed length decoder

---
+++ netty-demo/src/main/java/cn/howardliu/demo/nio/netty_nio/fixedLengthDecoder/EchoServer.java
@@ -0,0 +1,59 @@
+package cn.howardliu.demo.nio.netty_nio.fixedLengthDecoder;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * <br>created at 17-4-7
+ *
+ * @author liuxh
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class EchoServer {
+    public void bind(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new FixedLengthFrameDecoder(20));
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            b.bind(port).sync()
+                    .channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException ignore) {
+            }
+        }
+        new EchoServer().bind(port);
+    }
+}
---
+++ netty-demo/src/main/java/cn/howardliu/demo/nio/netty_nio/fixedLengthDecoder/EchoServerHandler.java
@@ -0,0 +1,26 @@
+package cn.howardliu.demo.nio.netty_nio.fixedLengthDecoder;
+
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * <br>created at 17-4-7
+ *
+ * @author liuxh
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Receive client: [" + msg + "]");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
