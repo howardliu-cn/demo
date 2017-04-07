df1375bb34da360e455cbc8735b763bc77a2986b
howardliu-cn <liuxinghao1988@gmail.com>
2017-04-07 14:27:49 +0800

    netty nio demo

---
+++ netty-demo/src/main/java/cn/howardliu/demo/nio/netty_nio/TimeClientHandler.java
@@ -0,0 +1,43 @@
+package cn.howardliu.demo.nio.netty_nio;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * <br>created at 17-4-7
+ *
+ * @author liuxh
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+    private final ByteBuf firstMessage;
+
+    public TimeClientHandler() {
+        byte[] req = "QUERY TIME ORDER".getBytes();
+        firstMessage = Unpooled.buffer(req.length);
+        firstMessage.writeBytes(req);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(firstMessage);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8");
+        System.out.println("Now is: " + body);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.err.println("Unexpected exception from downstream: " + cause.getMessage());
+        ctx.close();
+    }
+}
---
+++ netty-demo/src/main/java/cn/howardliu/demo/nio/netty_nio/TimeServerHandler.java
@@ -0,0 +1,41 @@
+package cn.howardliu.demo.nio.netty_nio;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.Date;
+
+/**
+ * <br>created at 17-4-7
+ *
+ * @author liuxh
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8");
+        System.out.println(Thread.currentThread().getName() + ", The time server receive order: " + body);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date().toString() : "BAD ORDER";
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.write(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+        ctx.close();
+    }
+}
